f27bcfe4ca03f7abec31200cb0e1f85da2775f80
ZicoRazzi <zicorazzi@gmail.com>
2021-11-18 11:58:14 +0100

    burger updated

--- js/main.js
+++ js/main.js
@@ -3,10 +3,55 @@ const iconMenu = document.querySelector('.menu_icon');
 const menuBody = document.querySelector('.menu_body');
 
 if (iconMenu) {
-  
-  iconMenu.addEventListener("click", function(e) {
+  iconMenu.addEventListener('click', function (e) {
     document.body.classList.toggle('_lock');
     iconMenu.classList.toggle('_active');
     menuBody.classList.toggle('_active');
   });
 }
+//scroll
+const menuLinks = document.querySelectorAll('.menu_link[data-goto]');
+if (menuLinks.length > 0) {
+  menuLinks.forEach((menuLink) => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if (
+      menuLink.dataset.goto &&
+      document.querySelector(menuLink.dataset.goto)
+    ) {
+      const gotoBlock = document.querySelector(menuLink.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top +
+        pageYOffset -
+        document.querySelector('.header_inner').offsetHeight;
+
+      if (iconMenu.classList.contains('_active')) {
+        document.body.classList.remove('_lock');
+        iconMenu.classList.remove('_active');
+        menuBody.classList.remove('_active');
+      }
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      });
+      e.preventDefault();
+    }
+  }
+}
+
+var header = querySelector('.header_inner');
+scrollPrev = 0;
+
+document.querySelector(window).scroll(function () {
+  var scrolled = document.querySelector(window).scrollTop;
+
+  if (scrolled > 100 && scrolled > scrollPrev) {
+    header.classList.add('out');
+  } else {
+    header.removeClass('out');
+  }
+  scrollPrev = scrolled;
+});
